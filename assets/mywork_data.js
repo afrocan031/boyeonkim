1fa538e3103d4548168b8659a8ece5af9e3d8a8c
afrocan031 <afrocan031@gmail.com>
2025-06-13 16:35:45 -0700

    Updates

---
+++ assets/mywork_data.js
@@ -0,0 +1,29 @@
+import project1_img from '../assets/helpit_00.png'
+import project2_img from '../assets/pr_00.png'
+import project3_img from '../assets/lib_00.png'
+import project4_img from '../assets/project_4.svg'
+import project5_img from '../assets/project_5.svg'
+import project6_img from '../assets/project_6.svg'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"Web design",
+        w_img:project1_img,
+        url: "/project1"
+    },
+    {
+        w_no:2,
+        w_name:"Web design",
+        w_img:project2_img,
+        url: "/project2"
+    },
+    {
+        w_no:3,
+        w_name:"Web design",
+        w_img:project3_img,
+        url: "/project3"
+    }
+]
+ 
+export default mywork_data;
